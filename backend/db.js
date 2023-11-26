2b6b0f89498a70c19ca2dabe69ab589ae8e8408f
Chie <man.chiehim@gmail.com>
2023-11-26 04:36:02 +0100

    stfuuuuu

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,49 @@
-// db.js
+const express = require("express")
+const mysql = require("mysql2")
+const cors = require("cors")
 
-const mysql = require('mysql2');
-require('dotenv').config();
+const app = express()
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+app.use(express.json())
+app.use(cors())
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  database: "login",         
+  password: "root", 
+})
+
+app.post("/register", (req, res) => {
+
+  const username = req.body.username
+  const password = req.body.password
+  const email = req.body.email
+
+  db.query("INSERT INTO users (username, password, email) VALUES (?,?,?)", [username, password, email], (err, result) => { console.log(err); });
+
+})
+app.listen(3001, () => {
+  console.log("running serer")
+})
+
+app.post("/login", (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+
+  db.query("SELECT * FROM users WHERE password = ? AND email = ?", 
+    [password, email],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      } else {
+        if (result.length > 0) {
+          res.send(result);
+        } else {
+          res.send({ message: "Wrong email/password combination!" });
+        }
+      }
+    }
+  );
 });
 
-module.exports = pool.promise();
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-// backend/server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const indexRoute = require('./routes/index');
 const db = require('./db');
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 
 // API routes
 app.use('/api/user', userRoutes);
+app.use('/', indexRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
